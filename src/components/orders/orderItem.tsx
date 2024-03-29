aedcf3794ab021826f201e3156201d3c8ffa1938
jjuggy <amidavid01@gmail.com>
2024-03-29 14:07:23 +0100

    updated the ongoing tab

---
+++ src/components/orders/orderItem.tsx
@@ -0,0 +1,144 @@
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Paragraph} from '@components/text/text';
+import {FlexedView} from '@components/view';
+import sharedImages from '@utility/sharedImages';
+import {AppButton} from '@components/button';
+
+const OrderItem = ({orders}) => {
+  const [numberOfOrders, setNumberOfOrders] = React.useState(1);
+  return orders?.map(order => {
+    return (
+      <View>
+        <View style={styles.container}>
+          <FlexedView justifyContent="space-between">
+            <FlexedView>
+              <Image
+                source={sharedImages.homeScreenDealImg}
+                style={{
+                  width: 90,
+                  height: 90,
+                  borderRadius: 20,
+                }}
+              />
+              <View style={{flexDirection: 'column', marginLeft: 12}}>
+                <Paragraph fontSize={12} style={{color: '#B1B1B1'}}>
+                  {order.storeName}
+                </Paragraph>
+                <Paragraph
+                  fontWeight="500"
+                  fontSize={15}
+                  style={{
+                    color: '#494949',
+                    marginVertical: 5,
+                  }}>
+                  {order.productName}
+                </Paragraph>
+                <Paragraph
+                  style={{
+                    color: '#1E89DD',
+                  }}
+                  fontSize={19}
+                  fontWeight="800">
+                  <Image
+                    tintColor={'#1E89DD'}
+                    style={{
+                      width: 15,
+                      height: 15,
+                      marginRight: 5,
+                    }}
+                    source={sharedImages.icons['naira']}
+                  />
+                  {order.price}
+                </Paragraph>
+              </View>
+            </FlexedView>
+            <View
+              style={{
+                backgroundColor: 'white',
+                borderRadius: 7,
+                flexDirection: 'row',
+                paddingHorizontal: 6,
+                marginTop: 'auto',
+                paddingVertical: 4,
+              }}>
+              <Pressable
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                onPress={() => {
+                  numberOfOrders > 0 && setNumberOfOrders(num => num - 1);
+                }}>
+                <Image
+                  tintColor={'#1E89DD'}
+                  style={{
+                    width: 15,
+                    height: 2,
+                    marginRight: 5,
+                  }}
+                  source={sharedImages.icons['minus']}
+                />
+              </Pressable>
+              <Paragraph
+                fontSize={16}
+                style={{
+                  marginHorizontal: 10,
+                }}>
+                {numberOfOrders}
+              </Paragraph>
+              <Pressable
+                onPress={() => {
+                  setNumberOfOrders(num => num + 1);
+                }}>
+                <Image
+                  tintColor={'#1E89DD'}
+                  style={{
+                    width: 16,
+                    height: 16,
+                    marginLeft: 5,
+                  }}
+                  source={sharedImages.icons['add']}
+                />
+              </Pressable>
+            </View>
+          </FlexedView>
+        </View>
+        <FlexedView style={{marginTop: 25}} justifyContent="space-between">
+          <Paragraph>Total</Paragraph>
+          <FlexedView>
+            <Image
+              tintColor={'#1E89DD'}
+              style={{
+                width: 15,
+                height: 15,
+                marginRight: 5,
+              }}
+              source={sharedImages.icons['naira']}
+            />
+            <Paragraph
+              style={{
+                color: '#1E89DD',
+              }}
+              fontSize={19}
+              fontWeight="800">
+              {order.price}
+            </Paragraph>
+          </FlexedView>
+        </FlexedView>
+      </View>
+    );
+  });
+};
+
+export default OrderItem;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    backgroundColor: '#EEEEF1',
+    borderRadius: 14,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+  },
+});
--- src/screens/orders/components/OngoingTab.tsx
+++ src/screens/orders/components/OngoingTab.tsx
@@ -1,16 +1,123 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React from 'react';
 import {Paragraph} from '@components/text/text';
-import {Spacer} from '@components/view';
+import {FlexedView, Spacer} from '@components/view';
+import colors from '@utility/colors';
+import sharedImages from '@utility/sharedImages';
+import OrderItem from '@components/orders/orderItem';
+import data from '../../../data';
 
 const OngoingTab = () => {
+  const [code, setCode] = React.useState<string[]>(['1', '2', '3', '4']);
+  const {orders} = data;
   return (
     <SafeAreaView>
-      <Spacer />
-      <Paragraph>
-        Provide the Seller with the code after inspection and your satisfied
-        with the item, to finalize purchase
-      </Paragraph>
+      <ScrollView style={{height: '100%'}}>
+        <Spacer />
+        <Paragraph>
+          Provide the seller with the code after inspection and you're satisfied
+          with the item, to finalize purchase
+        </Paragraph>
+        <Spacer />
+        <FlexedView
+          style={{paddingHorizontal: 20}}
+          justifyContent="space-between">
+          {code.map(cd => (
+            <View
+              style={{
+                borderRadius: 12,
+                width: 60,
+                height: 60,
+                backgroundColor: '#00ABC1',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Paragraph
+                style={{color: 'white'}}
+                fontSize={22}
+                fontWeight="500">
+                {cd}
+              </Paragraph>
+            </View>
+          ))}
+        </FlexedView>
+        <Spacer />
+
+        <View
+          style={{
+            flexDirection: 'column',
+          }}>
+          <View
+            style={{
+              width: '100%',
+            }}>
+            <Pressable
+              style={{
+                width: 'auto',
+                marginLeft: 'auto',
+              }}
+              onPress={() => {
+                console.warn('cancel order');
+              }}>
+              <Paragraph
+                style={{
+                  marginLeft: 'auto',
+                  color: 'red',
+                }}>
+                Cancel Order
+              </Paragraph>
+            </Pressable>
+          </View>
+          <View
+            style={{
+              flexDirection: 'column',
+            }}>
+            <Paragraph fontSize={16} fontWeight="600">
+              Pickup Details
+            </Paragraph>
+            <Paragraph
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Image
+                source={sharedImages.icons.location}
+                tintColor={'#707070'}
+                style={{
+                  width: 12,
+                  height: 12,
+                }}
+              />
+              <Paragraph
+                style={{
+                  color: '#707070',
+                  marginLeft: 5,
+                }}>
+                17, Abudu street, Abule oja, Lagos
+              </Paragraph>
+            </Paragraph>
+          </View>
+          <View
+            style={{
+              flexDirection: 'column',
+              marginTop: 18,
+            }}>
+            <Paragraph fontSize={16} fontWeight="600">
+              Your Order
+            </Paragraph>
+            <OrderItem orders={orders} />
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
