ca8468706564982019ca8e298b370d33bd1e2506
jjuggy <amidavid01@gmail.com>
2024-03-29 15:31:04 +0100

    completed tab is done

--- src/screens/orders/components/CompletedTab.tsx
+++ src/screens/orders/components/CompletedTab.tsx
@@ -1,11 +1,81 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {FlexedView, Spacer} from '@components/view';
+import {Paragraph} from '@components/text/text';
 
+interface ICompleteOrder {
+  orderNumber: string;
+  orderItems: string[];
+  orderDate: string;
+  orderStatus: string;
+  storeName: string;
+  cancelled: boolean;
+}
 const CompletedTab = () => {
+  const completedOrders: ICompleteOrder[] = [
+    {
+      orderNumber: '32453',
+      orderItems: ['1', '2', '3'],
+      orderDate: '27th August 2021, 12:00',
+      orderStatus: 'completed',
+      storeName: 'OJB Declutter',
+      cancelled: false,
+    },
+    {
+      orderNumber: '32453',
+      orderItems: ['1', '2', '3'],
+      orderDate: '27th August 2021, 12:00',
+      orderStatus: 'completed',
+      storeName: 'OJB Declutter',
+      cancelled: true,
+    },
+  ];
+  const completeView = (details: ICompleteOrder) => {
+    return (
+      <FlexedView
+        justifyContent="space-between"
+        style={{
+          borderBottomWidth: 0.5,
+          borderBottomColor: '#D2D2D2',
+          paddingVertical: 16,
+        }}>
+        <View style={{flexDirection: 'column'}}>
+          <Paragraph style={{color: '#737373'}} fontSize={15} fontWeight="600">
+            {details.storeName}
+          </Paragraph>
+          <Spacer />
+          <Paragraph color="#737373" fontSize={13} fontWeight="300">
+            {details.orderDate}
+          </Paragraph>
+        </View>
+        <View
+          style={{
+            flexDirection: 'column',
+            alignItems: 'flex-end',
+          }}>
+          {details.cancelled ? (
+            <Paragraph color="#FF0000" fontSize={13} fontWeight="300">
+              Cancelled
+            </Paragraph>
+          ) : (
+            <Paragraph color="#737373" fontSize={13} fontWeight="300">
+              Order {details.orderNumber}
+            </Paragraph>
+          )}
+          <Spacer />
+          <Paragraph color="#737373" fontSize={12} fontWeight="300">
+            View details
+          </Paragraph>
+        </View>
+      </FlexedView>
+    );
+  };
   return (
-    <View>
-      <Text>CompletedTab</Text>
-    </View>
+    <ScrollView style={{height: '100%'}}>
+      {completedOrders.map(completedOrder => {
+        return completeView(completedOrder);
+      })}
+    </ScrollView>
   );
 };
 
