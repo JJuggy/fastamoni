289be1457f373ae25f7774b6ee7088e535b27b4b
Amiolemen David <david@piqy.co.uk>
2024-05-07 11:28:40 +0100

    update on the cart item

--- src/screens/orders/Checkout.tsx
+++ src/screens/orders/Checkout.tsx
@@ -26,12 +26,20 @@ import {useModal} from '@providers/DynamicModalProvider';
 import {windowHeight, writeToClipboard} from '@utility/helpers';
 import {HomeNavigatorParams} from 'src/types';
 import {useNavigation} from '@react-navigation/native';
+import {useCart} from '@store/cart/hook';
 
 const Checkout = () => {
   const {navigate} = useNavigation<HomeNavigatorParams>();
   const [payMethod, setPayMethod] = useState('card');
   const {close, show} = useModal();
-
+  const {cart} = useCart();
+  const itemDiscount = 20000;
+  const couponDiscount = 8000;
+  let totalPrice: number = 0;
+  cart.reduce((total, product: any) => {
+    return (totalPrice = total + product?.product.price * product?.quantity);
+  }, 0);
+  const totalAfterDiscount = totalPrice - itemDiscount - couponDiscount;
   const TransferModalV = () => {
     return (
       <BaseView background={colors.white}>
@@ -114,13 +122,15 @@ const Checkout = () => {
               </Paragraph>
             </View>
             <Spacer />
-            <FlexedView justifyContent="space-between">
-              <FlexedView>
-                <Paragraph>1x</Paragraph>
-                <Paragraph>3.5kva Elepaq Generator</Paragraph>
+            {cart.map(item => (
+              <FlexedView justifyContent="space-between">
+                <FlexedView>
+                  <Paragraph>{item.quantity}x</Paragraph>
+                  <Paragraph>{item.product_title}</Paragraph>
+                </FlexedView>
+                <Paragraph>{`${NAIRA}${item?.product?.price}`}</Paragraph>
               </FlexedView>
-              <Paragraph>{`${NAIRA}150,000.00`}</Paragraph>
-            </FlexedView>
+            ))}
             <Spacer height={40} />
             <View>
               <Paragraph fontSize={18} fontWeight="600">
@@ -129,24 +139,24 @@ const Checkout = () => {
               <Spacer />
               <FlexedView justifyContent="space-between">
                 <Paragraph>Order Total</Paragraph>
-                <Paragraph>{`${NAIRA}150,000.00`}</Paragraph>
+                <Paragraph>{`${NAIRA}${totalPrice}`}</Paragraph>
               </FlexedView>
               <Spacer height={10} />
               <FlexedView justifyContent="space-between">
                 <Paragraph>Item discount</Paragraph>
-                <Paragraph>{`${NAIRA}0.00`}</Paragraph>
+                <Paragraph>{`${NAIRA}${itemDiscount}`}</Paragraph>
               </FlexedView>
               <Spacer height={10} />
               <FlexedView justifyContent="space-between">
                 <Paragraph>Coupon Discount</Paragraph>
-                <Paragraph>{`${NAIRA}0.00`}</Paragraph>
+                <Paragraph>{`${NAIRA}${couponDiscount}`}</Paragraph>
               </FlexedView>
               <Spacer height={27} />
               <Divider height={2} />
               <Spacer height={15} />
               <FlexedView justifyContent="space-between">
                 <Paragraph>Total</Paragraph>
-                <Paragraph>{`${NAIRA}150,000.00`}</Paragraph>
+                <Paragraph>{`${NAIRA}${totalAfterDiscount}`}</Paragraph>
               </FlexedView>
             </View>
             <Spacer height={30} />
